9dfd6e48122fdfcbe193eba88023d204c3d072c4
pavan987 <gondhi@usc.edu>
2018-06-09 21:51:26 -0700

    added styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './Home.js'
 import Usersignup from './Usersignup.js'
 import Signup from './Signup.js'
+import Signin from './Signin.js'
 import Event from './Event.js'
 import Dashboard from './Dashboard.js'
 import Userdashboard from './Userdashboard.js'
@@ -17,7 +18,8 @@ class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/usersignup' component={Usersignup} />
         <Route path='/signup' component={Signup} />
-        <Route path='/dashboard' component={Dashboard} /> 
+        <Route path='/signin' component={Signin} />
+        <Route path='/dashboard' component={Dashboard} />
         <Route path='/events' component={Event} />
         <Route path='/userdashboard' component={Userdashboard} />
       </Switch>
@@ -27,5 +29,3 @@ class App extends Component {
 }
 
 export default App;
-
-
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap'
+import { Container, Row, Col, Button } from 'reactstrap'
 
 import { Jumbotron } from 'reactstrap'
 
@@ -10,8 +10,18 @@ export default class Home extends Component {
 		     <div>
 		     	<Jumbotron fluid>
 		     		<Container fluid>
+							<Row><Col sm={{ size: 6, order: 2, offset: 3 }}>
 		     			<h1 className=""> Greenr </h1>
-		     			<p> </p>
+							</Col></Row>
+							<Row><Col>
+							<h2> Company Admin </h2>
+							<a href="/signup"><Button color="primary">Signup</Button></a>{' '}
+	        		<a href="/signin"><Button color="success">Signin</Button></a>{' '}
+							</Col><Col>
+							<h2> Company User </h2>
+							<a href="/usersignup"><Button color="primary">Signup</Button></a>{' '}
+	        		<a href="/signin"><Button color="success">Signin</Button></a>{' '}
+						</Col></Row>
 		     		</Container>
 		     	</Jumbotron>
 		     </div>
